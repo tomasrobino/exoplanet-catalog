cb0d8f2296590da295239f1729a3aa8698dfdaa5
Tomas <tomaserobino@gmail.com>
2024-08-19 23:47:19 +0200

    successful fetch query

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import './App.css'
+import {useEffect} from "react";
 
 function App() {
   //Select star system or planet
   //Show start and planets orbiting it graphically
+  const url: string = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?";
+
+  useEffect(() => {
+    fetch("https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative&where=koi_disposition like 'CANDIDATE' and koi_period>300 and koi_prad<2")
+      .then(response => response.text())
+      .then(response => console.log(response))
+  }, [])
 
   return (
     <>
